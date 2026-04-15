4e96edfba1b02f3f6482479f7b89ba66390880cc
apurva011104 <akaul7817@gmail.com>
2026-04-15 10:21:50 +0530

    [Apurva Gupta] Add. Added global exception handler

--- paymentservice/src/main/java/com/quickbite/payment/paymentservice/exception/GlobalExceptionHandler.java
+++ paymentservice/src/main/java/com/quickbite/payment/paymentservice/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.quickbite.payment.paymentservice.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,5 +31,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleGeneric(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Something went wrong");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+    
+        Map<String, String> errors = new HashMap<>();
+    
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+    
+        return ResponseEntity.badRequest().body(errors);
     }
 }
